75dab792b8931a00ab376ed58df5c4c3b990a5c2
Nikola Crhák <nikola.crhak@gmail.com>
2025-10-09 08:39:12 +0200

    Added nahodne pole

---
+++ mycodes/nahodne_pole.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    srand(time(NULL));
+
+    char arr[5];
+
+    for (int i = 0; i < 5; i++) {
+        arr[i] = rand() % ('z' - 'a' + 1) + 'a';
+        printf("%c ", arr[i]);
+    }
+
+    printf("\n");
+    return 0;
+}
