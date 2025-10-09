fe6bd2e8e4e009326c5b1a51f52455f3b99ee515
Nikola Crhák <nikola.crhak@gmail.com>
2025-10-09 09:17:50 +0200

    Improved random pass

--- mycodes/random_pass.cpp
+++ mycodes/random_pass.cpp
@@ -7,11 +7,20 @@ int main() {
 
     int randLen = rand() % (12 - 7 + 1) + 7;
 
-    char arr[12];
+    char *arr = (char *)malloc(randLen * sizeof(char));
+
+    if (arr == NULL) {
+        perror("Chyba alokace pameti\n");
+        return 1;
+    }
 
     for (int i = 0; i < randLen; i++) {
         arr[i] = rand() % ('}' - '!' - 1) + '!';
 
         printf("%c", arr[i]);
     }
+
+    free(arr);
+
+    return 0;
 }
