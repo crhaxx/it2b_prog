a75a21ea6ea43eecc534139b3f106f2eb1fa792e
Nikola Crhák <122156533+crhaxx@users.noreply.github.com>
2026-03-05 16:13:39 +0100

    Added array

---
+++ c#codes/ArrayTest.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[] ints = new int[10];
+
+        for (int i = 0; i < ints.Length; i++)
+        {
+            Console.Write("Enter num: ");
+            ints[i] = int.Parse(Console.ReadLine());
+        }
+
+
+        Array.Sort(ints);
+        Console.WriteLine("--------");
+        Console.WriteLine($"Maximum: {ints.Max()}");
+        Console.WriteLine($"Min Max dif: {ints.Max() - ints.Min()}");
+        Console.WriteLine($"3 smallest: {ints[0]}, {ints[1]}, {ints[2]}");
+        Console.WriteLine($"Median: {(ints[5] + ints[6]) / 2}");
+    }
+}
---
+++ c#codes/PoleFunkce.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static bool jsouKladne(int[] arr)
+    {
+        foreach (int cislo in arr)
+        {
+            if (cislo % 2 != 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static void Main()
+    {
+        int[] ints = new int[10];
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write("Zadej cislo: ");
+            ints[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine($"{jsouKladne(ints)}");
+        Console.WriteLine($"{serazene(ints)}");
+    }
+}
